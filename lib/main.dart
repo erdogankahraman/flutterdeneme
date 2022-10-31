80b039892f8e4d36514ab0e749d245b94801dc47
FalconDev <erdogankahraman0205@gmail.com>
2022-10-31 19:46:39 +0300

    ilk kodum

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Merhaba Patron"),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: const <Widget>[
+              Text("Resim 22221"),
+              WebView(
+                initialUrl: 'https://www.tutorialkart.com/',
+                javascriptMode: JavascriptMode.unrestricted,
+              ),
+              Image(
+                image: NetworkImage(
+                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+              ),
+              Text("Resim2"),
+              Image(
+                image: NetworkImage(
+                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  ));
+}
