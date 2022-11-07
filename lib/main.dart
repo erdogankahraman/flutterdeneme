43c644df21b6757e1ecd31a2a1848a7df146223f
FalconDev <erdogankahraman0205@gmail.com>
2022-11-07 19:57:41 +0300

    3. ekran eklendi. toggle butonlar eklendi.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
+final ButtonStyle flatButtonStyle = TextButton.styleFrom(
+  foregroundColor: Colors.black87,
+  backgroundColor: Colors.red,
+  minimumSize: const Size(188, 50),
+  elevation: 18,
+  padding: EdgeInsets.symmetric(horizontal: 16.0),
+  shape: const RoundedRectangleBorder(
+    borderRadius: BorderRadius.all(Radius.circular(12.0)),
+  ),
+);
+
 void main() {
   runApp(MaterialApp(
     home: FirstRoute(),
@@ -9,17 +20,6 @@ void main() {
 
 class FirstRoute extends StatelessWidget {
   FirstRoute({super.key});
-
-  final ButtonStyle flatButtonStyle = TextButton.styleFrom(
-    foregroundColor: Colors.black87,
-    backgroundColor: Colors.red,
-    minimumSize: const Size(188, 50),
-    elevation: 18,
-    padding: EdgeInsets.symmetric(horizontal: 16.0),
-    shape: const RoundedRectangleBorder(
-      borderRadius: BorderRadius.all(Radius.circular(12.0)),
-    ),
-  );
 
   @override
   Widget build(BuildContext context) {
@@ -60,8 +60,28 @@ class FirstRoute extends StatelessWidget {
                       );
                       Navigator.push(
                         context,
-                        MaterialPageRoute(
-                            builder: (context) => SecondRoute()),
+                        MaterialPageRoute(builder: (context) => SecondRoute()),
+                      );
+                    },
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(25),
+                  child: TextButton(
+                    style: flatButtonStyle,
+                    child: Text(
+                      'Button 3',
+                      style: TextStyle(color: Colors.white.withOpacity(0.6)),
+                    ),
+                    onPressed: () {
+                      Fluttertoast.showToast(
+                        msg: "This is a Toast message", // message
+                        toastLength: Toast.LENGTH_SHORT, // length
+                        gravity: ToastGravity.CENTER, // location// duration
+                      );
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => ThirdRoute()),
                       );
                     },
                   ),
@@ -89,9 +109,70 @@ class SecondRoute extends StatelessWidget {
           onPressed: () {
             Navigator.pop(context);
           },
-          child: const Text('Go back!'),
+          child: Column(children: <Widget>[
+            const Text('Go back!'),
+            Container(
+              margin: const EdgeInsets.all(25),
+              child: TextButton(
+                style: flatButtonStyle,
+                child: Text(
+                  'Button 3',
+                  style: TextStyle(color: Colors.white.withOpacity(0.6)),
+                ),
+                onPressed: () {
+                  Fluttertoast.showToast(
+                    msg: "This is a Toast message", // message
+                    toastLength: Toast.LENGTH_SHORT, // length
+                    gravity: ToastGravity.CENTER, // location// duration
+                  );
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ThirdRoute()),
+                  );
+                },
+              ),
+            ),
+          ]),
         ),
       ),
     );
+  }
+}
+
+class ThirdRoute extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<ThirdRoute> {
+  List<bool> _selections = List.generate(3, (_) => false);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Flutter - tutorialkart.com')),
+      ),
+      body: ListView(children: <Widget>[
+        Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(20),
+            child: ToggleButtons(
+              children: <Widget>[
+                Icon(Icons.add_comment),
+                Icon(Icons.airline_seat_individual_suite),
+                Icon(Icons.add_location),
+              ],
+              isSelected: _selections,
+              onPressed: (int index) {
+                setState(() {
+                  _selections[index] = !_selections[index];
+                });
+              },
+            ))
+      ]),
+    ));
   }
 }
